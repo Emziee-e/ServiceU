b02204e24853400bc227f1838c24b751238add3e
Tristan-jay12 <2300524@ub.edu.ph>
2025-12-05 02:32:45 +0800

    added choose profile js

--- StackNavigation.js
+++ StackNavigation.js
@@ -5,6 +5,7 @@ import customerSignUp1 from './customerSignUp1';
 import customerSignUp2 from './customerSignUp2';
 import CustomerSignUp3 from './customerSignUp3';
 import ChooseProfile from './ChooseProfile';
+import RatingReview from './RatingReview';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,13 +13,14 @@ const Stack = createNativeStackNavigator();
 const StackNavigation = () => {
   return (
     <Stack.Navigator
-        initialRouteName='ChooseProfile'
+        initialRouteName='RatingReview'
         screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="ChooseProfile" component={ChooseProfile} />  
+      <Stack.Screen name="RatingReview" component={RatingReview} />  
       <Stack.Screen name="customerSignUp" component={customerSignUp1} />
       <Stack.Screen name="customerSignUp2" component={customerSignUp2} />
       <Stack.Screen name="customerSignUp3" component={CustomerSignUp3} />
+      <Stack.Screen name="ChooseProfile" component={ChooseProfile} />
     </Stack.Navigator>
   );
 };
