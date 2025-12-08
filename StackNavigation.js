29e19672f91f76633b99e037fc243507922822f5
Tristan-jay12 <2300524@ub.edu.ph>
2025-12-08 16:57:14 +0800

    added ActivityScreen1

--- StackNavigation.js
+++ StackNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Activity } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import customerSignUp1 from './customerSignUp1';
@@ -6,6 +6,7 @@ import customerSignUp2 from './customerSignUp2';
 import CustomerSignUp3 from './customerSignUp3';
 import ChooseProfile from './ChooseProfile';
 import RatingReview from './RatingReview';
+import ActivityScreen1 from './ActivityScreen1';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,14 +14,15 @@ const Stack = createNativeStackNavigator();
 const StackNavigation = () => {
   return (
     <Stack.Navigator
-        initialRouteName='ChooseProfile'
+        initialRouteName='ActivityScreen1'
         screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="ChooseProfile" component={ChooseProfile} />  
+    > 
+      <Stack.Screen name="ActivityScreen1" component={ActivityScreen1} />  
       <Stack.Screen name="customerSignUp" component={customerSignUp1} />
       <Stack.Screen name="customerSignUp2" component={customerSignUp2} />
       <Stack.Screen name="customerSignUp3" component={CustomerSignUp3} />
       <Stack.Screen name="RatingReview" component={RatingReview} />
+      <Stack.Screen name="ChooseProfile" component={ChooseProfile} />
     </Stack.Navigator>
   );
 };
